c330e0d64c04b568e05a1b5fcaa0dfb5577bc917
bkosees <39088348+bkosees@users.noreply.github.com>
2021-08-05 09:01:04 +0000

    Add files via upload

---
+++ user.sql
@@ -0,0 +1,21 @@
+USE [BIBLIO_DB]
+GO
+
+/****** Object:  Table [dbo].[User]    Script Date: 03/08/2021 15:10:55 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[User](
+	[UserID] [int] IDENTITY(1,1) NOT NULL,
+	[userName] [varchar](50) NULL,
+	[Passwd] [varchar](50) NULL,
+ CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED 
+(
+	[UserID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
